c8377a892da5d3b7183e9bc7901779c18510e9db
yesol <yeahsol.kwon@gmail.com>
2024-09-21 10:40:38 +1000

    added view logic

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -1,3 +1,42 @@
+# Create your views here.
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
+from .models import Project
+from .serializers import ProjectSerializer
+
+class ProjectList(APIView):
+
+    def get(self, request):
+       projects = Project.objects.all()
+       serializer = ProjectSerializer(projects, many=True)
+       return Response(serializer.data)
+    
+    def post(self,request):
+        serializer=ProjectSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+class ProjectDetail(APIView):
+    def get_object(self,pk):
+        try:
+            project=Project.objects.get(pk=pk)
+            return project
+        except Project.DoesNotExist:
+            raise Http404
+    def get(self,request,pk):
+        project=self.get_object(pk)
+        serializer=ProjectSerializer(project)
+        return Response(serializer.data)
